2382ea4d6bf717295b1ab6d48bde04e9c5e25369
Nelly Nikitina <worldpiece000@gmail.com>
2023-08-04 03:25:04 +0300

    feat: realise dynamic pagination

--- src/pages/Posts.jsx
+++ src/pages/Posts.jsx
@@ -1,15 +1,16 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { usePosts } from "../hooks/usePosts";
 import { PostsFilter } from "../components/PostsFilter";
 import { MyButton } from "../components/UI/button/MyButton";
 import { MyModal } from "../components/UI/modal/MyModal";
 import { PostForm } from "../components/PostForm";
 import PostsList from "../components/PostsList";
-import { Pagination } from "../components/UI/pagination/Pagination";
 import { Loader } from "../components/UI/loader/Loader";
 import { useFetching } from "../hooks/useFetching";
 import PostService from "../API/PostService";
 import { getPagesCount } from "../utils/pages";
+import { useObserver } from "../hooks/useObserver";
+import { MySelect } from "../components/UI/select/MySelect";
 
 export function Posts() {
   const [posts, setPosts] = useState([]);
@@ -18,11 +19,12 @@ export function Posts() {
   const [page, setPage] = useState(1);
   const [filter, setFilter] = useState({ sort: "", query: "" });
   const [toShowModal, setToShowModal] = useState(false);
+  const lastElement = useRef();
 
   const [fetchPosts, arePostsLoading, postError] = useFetching(
     async function fetchPosts(limit, page) {
       const response = await PostService.getAll(limit, page);
-      setPosts(response.data);
+      setPosts([...posts, ...response.data]);
       const totalPostsCount = +response.headers["x-total-count"];
       setTotalPages(getPagesCount(totalPostsCount, limit));
     }
@@ -32,12 +34,11 @@ export function Posts() {
 
   useEffect(() => {
     fetchPosts(limit, page);
-  }, []);
+  }, [page, limit]);
 
-  const changePage = (page) => {
-    setPage(page);
-    fetchPosts(limit, page);
-  };
+  useObserver(lastElement, page < totalPages, arePostsLoading, () => {
+    setPage(page + 1);
+  });
 
   const createPost = (newPost) => {
     setPosts([...posts, newPost]);
@@ -61,36 +62,43 @@ export function Posts() {
       </MyModal>
       <hr style={{ margin: "10px 0" }} />
       <PostsFilter filter={filter} setFilter={setFilter} />
+      <MySelect
+        value={limit}
+        onChange={(value) => setLimit(+value)}
+        defaultValue="Количество постов"
+        options={[
+          { value: 5, text: "5" },
+          { value: 10, text: "10" },
+          { value: 25, text: "25" },
+          { value: -1, text: "Показать всё" },
+        ]}
+      />
+
+      <PostsList
+        title={"Языки погромирования"}
+        posts={filteredAndSortedPosts}
+        remove={removePost}
+      />
 
       {postError ? (
         <p style={{ marginTop: "20px", color: "red" }}>
           Произошла ошибка: {postError}
         </p>
-      ) : arePostsLoading ? (
-        <div
-          style={{
-            display: "flex",
-            justifyContent: "center",
-            marginTop: "50px",
-          }}
-        >
-          <Loader />
-        </div>
       ) : (
-        <>
-          <PostsList
-            title={"Языки погромирования"}
-            posts={filteredAndSortedPosts}
-            remove={removePost}
-          />
-
-          <Pagination
-            totalPages={totalPages}
-            currentPage={page}
-            changePage={changePage}
-          />
-        </>
+        arePostsLoading && (
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              marginTop: "50px",
+            }}
+          >
+            <Loader />
+          </div>
+        )
       )}
+
+      <div ref={lastElement} style={{ height: 50 }}></div>
     </div>
   );
 }
